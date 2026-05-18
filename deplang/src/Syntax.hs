050c41f9e725dc854c2e286ec658e0f9a36c7512
rbykov <rbykov04@gmail.com>
2026-05-18 13:09:31 +0300

    deplang : add subst and shift

--- deplang/src/Syntax.hs
+++ deplang/src/Syntax.hs
@@ -13,6 +13,34 @@ data Term
         | App Term Term
         deriving (Show, Eq)
 
+-- shift d c term: d - на сколько сдвигаем, c - текущая отсечка (уровень вложенности)
+shift :: Int -> Int -> Term -> Term
+shift d cutoff (Var index) =
+  if index >= cutoff
+  then Var (index + d)
+  else Var index
+shift d cutoff (Lam ty body) = Lam (shift d cutoff ty) (shift d (cutoff + 1) body)
+shift d cutoff (Pi  dom cod) = Pi (shift d cutoff dom) (shift d (cutoff + 1) cod)
+shift d cutoff (App fun arg) = App (shift d cutoff fun) (shift d cutoff arg)
+shift _ _ (Univ i) = Univ i
+
+-- subst t t': подставляет term t вместо переменной с индексом 0 в term t'
+-- n - что вставляем
+-- m - куда
+subst :: Term -> Term -> Term
+subst = substAt 0
+
+substAt :: Int -> Term -> Term -> Term
+substAt cutoff n (Var i) =
+        case compare i cutoff of
+          EQ -> shift cutoff 0 n
+          GT -> Var (i - 1)
+          LT -> Var i
+substAt cutoff term (Lam ty body) = Lam (substAt cutoff term ty) (substAt (cutoff + 1) term body)
+substAt cutoff term (Pi dom cod) = Pi (substAt cutoff term dom) (substAt (cutoff + 1) term cod)
+substAt cutoff term (App fun arg) = App (substAt cutoff term fun) (substAt cutoff fun arg)
+substAt _ _ (Univ i) = Univ i
+
 -- prittyPrint
 allNames :: [String]
 allNames = basic ++ [ n ++ show i | i <- [1..] ,  n <- basic  ]
@@ -31,7 +59,6 @@ prittyPrint ctx (Lam typeArg body) =
       tyStr = prittyPrint ctx typeArg
       bodyStr = prittyPrint (fresh : ctx) body
   in "λ(" ++ fresh ++ " : " ++ tyStr ++ "). " ++ bodyStr
-
 prittyPrint ctx (Pi dom cod) =
   let fresh = pickFreshName ctx
       domStr = prittyPrint ctx dom
@@ -56,3 +83,5 @@ bracketArg (App _ _) s = "(" ++ s ++ ")"
 bracketArg (Lam _ _) s   = "(" ++ s ++ ")"
 bracketArg (Pi _ _) s  = "(" ++ s ++ ")"
 bracketArg _ s         = s
+
+
--- deplang/test/CoreSpec.hs
+++ deplang/test/CoreSpec.hs
@@ -11,14 +11,51 @@ idType :: Term
 idType = Pi (Univ 0) (Pi (Var 0) (Var 1))
 
 spec :: Spec
-spec = describe "prittyPrint" $ do
-  it "6 имя" $ allNames !! 6 `shouldBe` "x1"
-  it "7 имя" $ allNames !! 7 `shouldBe` "y1"
-  it "возьмем свободное имя" $ pickFreshName ["x"] `shouldBe` "y"
-  it "возьмем свободное имя: зашли на круг" $ pickFreshName ["x", "y", "z", "v", "u", "w"] `shouldBe` "x1"
-  it "свободная переменная" $ prittyPrint [] (Var 0) `shouldBe` "free_0"
-  it "странная Lam" $ prittyPrint [] (Lam (Var 0) (Var 0)) `shouldBe` "λ(x : free_0). x"
-  it "Type0" $ prittyPrint [] (Univ 0) `shouldBe` "Type0"
-  it "тело id для зав типов" $ prittyPrint [] idTerm `shouldBe` "λ(x : Type0). λ(y : x). y"
-  it "тип id для зав типов" $ prittyPrint [] idType `shouldBe` "П(x : Type0). П(y : x). x"
-  it "странная аппликация" $ prittyPrint ["Bool"] (App (Var 0) (Var 0)) `shouldBe` "Bool Bool"
+spec = do
+  describe "prittyPrint" $ do
+    it "6 имя" $ allNames !! 6 `shouldBe` "x1"
+    it "7 имя" $ allNames !! 7 `shouldBe` "y1"
+    it "возьмем свободное имя" $ pickFreshName ["x"] `shouldBe` "y"
+    it "возьмем свободное имя: зашли на круг" $ pickFreshName ["x", "y", "z", "v", "u", "w"] `shouldBe` "x1"
+    it "свободная переменная" $ prittyPrint [] (Var 0) `shouldBe` "free_0"
+    it "странная Lam" $ prittyPrint [] (Lam (Var 0) (Var 0)) `shouldBe` "λ(x : free_0). x"
+    it "Type0" $ prittyPrint [] (Univ 0) `shouldBe` "Type0"
+    it "тело id для зав типов" $ prittyPrint [] idTerm `shouldBe` "λ(x : Type0). λ(y : x). y"
+    it "тип id для зав типов" $ prittyPrint [] idType `shouldBe` "П(x : Type0). П(y : x). x"
+    it "странная аппликация" $ prittyPrint ["Bool"] (App (Var 0) (Var 0)) `shouldBe` "Bool Bool"
+
+  describe "Сдвиг индексов (shift)" $ do
+      it "не меняет переменные ниже отсечки" $ do
+        -- В теле Lam индекс 0 связан. Сдвиг с отсечкой 1 не должен его трогать.
+        -- Lam (Univ 0) (Var 0) -> внутри Lam отсечка становится 1. Var 0 < 1, не меняется.
+        shift 1 1 (Lam (Univ 0) (Var 0)) `shouldBe` Lam (Univ 0) (Var 0)
+      it "сдвигает свободные переменные выше или равные отсечке" $ do
+        -- Индекс 1 внутри тела Lam свободен. При отсечке 0 он должен стать 2.
+        shift 1 0 (Lam (Univ 0) (Var 1)) `shouldBe` Lam (Univ 0) (Var 2)
+      it "НЕ увеличивает отсечку внутри аннотации типа лямбды" $ do
+        -- Переменная в типе аргумента находится снаружи связывателя.
+        -- Если стартовая отсечка 0, то Var 0 в типе должен сдвинуться, так как он свободен.
+        shift 1 0 (Lam (Var 0) (Var 0)) `shouldBe`  ( Lam (Var 1) (Var 0))
+
+  describe "Подстановка (subst)" $ do
+    it "заменяет целевую переменную Var 0" $ do
+      subst (Univ 0) (Var 0) `shouldBe` Univ 0
+
+    it "уменьшает индекс свободных переменных на 1" $ do
+      subst (Univ 0) (Var 1) `shouldBe` Var 0
+
+    it "правильно обрабатывает вложенность в Lam" $ do
+      -- Подставляем (Var 0) в выражение (Lam (Univ 0) (Var 1))
+      -- Внутри тела Lam целевой индекс смещается на +1 и становится равным 1.
+      -- Подставляемый терм (Var 0) сдвигается на +1 внутри Lam и становится (Var 1).
+      -- Итог: Var 1 в теле заменяется на Var 1.
+      subst (Var 0) (Lam (Univ 0) (Var 1)) `shouldBe` Lam (Univ 0) (Var 1)
+
+    it "комплексный тест: (\\x: A. x y) z -> z y" $ do
+      -- В индексах Де Брёйна:
+      -- Выражение: Lam (Univ 0) (App (Var 0) (Var 1)) -- (\x. x y)
+      -- Аргумент: Var 2 -- z
+      -- Ожидаем после подстановки аргумента в тело: App (Var 2) (Var 0)
+      let body = App (Var 0) (Var 1)
+      let arg  = Var 2
+      subst arg body `shouldBe` App (Var 2) (Var 0)
